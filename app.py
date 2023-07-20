d00333590a6d5c261de165c23cc87afaa56a43e7
Karly Stark <karlystark@gmail.com>
2023-07-20 12:00:53 -0700

    completed step one start page

--- app.py
+++ app.py
@@ -7,3 +7,21 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+@app.get("/")
+def home_render():
+    title = survey.title
+    instructions = survey.instructions
+
+    return render_template(
+        "survey_start.html",
+        title = title,
+        instructions = instructions
+    )
+
+@app.post("/begin")
+def redirect_start():
+    return redirect("/questions/0")
+
